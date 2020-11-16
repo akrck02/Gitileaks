58ba884d36844c0ebc2babc699ef36a1ee8072ca
aritzr <37852686+aritzr@users.noreply.github.com>
2020-11-16 09:52:35 +0100

    pantallazos y archivos

---
+++ #4 - Apache/Archivos configuracion/config.php
@@ -0,0 +1,16 @@
+<?php
+    define("DB_HOST", "localhost");
+    define("DB_USER", "db");
+    define("DB_PASS", "dw2");
+    define("DB_DATABASE", "subastas");
+    define("NOMFORO", "SUBASTAS TIMO");
+    define("RUTABASE", "https://subastas.atenea.olimpo.god/");
+    define("MONEDA", "€");
+    define("HOME", "index.php");
+    define("MSGREG", "<h1>CONFIRMAR REGISTRO</h1>"
+            . "<p>Para confirmar el registro es necesario que pinches en el siguiente enlace:</p>");
+    define("IMG", "img/");
+    define("MSGANUN", "<h1>PUJAS A PUNTITO DE CARAMELO</h1>"
+            . "<p>Puede que te interesen los siguientes items:</p>");
+    define("IMG", "img/");
+    define("ANUNCIANTES", "archivos/anunciantes.txt");
